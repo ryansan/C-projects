5978a43bded2458ebcdb1a5ab117ed9c54fc9694
ryansan <31653675+ryansan@users.noreply.github.com>
2018-05-27 23:17:36 +0200

    Initial Commit

---
+++ FirstProj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FirstProj
+//
+//  Created by Ryan Sangha on 27/05/2018.
+//  Copyright © 2018 Ryan Sangha. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
